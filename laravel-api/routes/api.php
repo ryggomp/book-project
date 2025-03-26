3e789e1bbeb2d78e374c20999a34be76aa5706b5
ryggomp <ryggomp7f@gmail.com>
2025-03-26 19:23:58 +0700

    2nd commit

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,5 +1,8 @@
 <?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\BookController;
 
 Route::get('/books', [BookController::class, 'index']);
-
+Route::post('/books', [BookController::class, 'store']);
